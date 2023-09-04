805775daac77ea622fe21eee37fbf16f2a53b44a
bongj9 <lollypark501@naver.com>
2023-09-04 11:16:56 +0900

    1번문제

--- src/_1/Student.java
+++ src/_1/Student.java
@@ -1,6 +1,7 @@
 package _1;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Student {
    private String name;  //멤버변수는 웬만해서 private로 하는게 좋다
@@ -33,9 +34,9 @@ public class Student {
 
 class main {
    public static void main(String[] args) {
-      Student stu1 = new Student();
-      Student stu2 = new Student();
-      Student stu3 = new Student();
+      Student stu1 = new Student("손오공","1682");
+      Student stu2 = new Student("저팔계", "1772");
+      Student stu3 = new Student("사오정", "1813");
 
 
       ArrayList<Student> list = new ArrayList<Student>();
@@ -43,6 +44,47 @@ class main {
       list.add(stu1);
       list.add(stu2);
       list.add(stu3);
+
+      for (Student stu : list) {
+         System.out.println(stu.getName());
+         System.out.println(stu.getNo());
+
+      }
+      Scanner scan = new Scanner(System.in);
+      while (true) {
+         System.out.println("계속 검색을 하고싶으시면 y, 종료하고싶으면 n");
+         boolean flag = false; //한번이라도 있었더라면 flag변수로 사용함 있으면 true로 반환
+
+         String input = scan.next();
+         if (input.equals("y")) {
+            System.out.println("검색을 시작합니다.");
+            String name = scan.next();//학생이름을 받기
+            for (Student stu : list) {
+               if (stu.getName().equals(name)) {  //Stu의 getname으로 반환한값이 name이랑 같냐를 확인함
+                  System.out.println("해당하는 학생의 학번은: " +
+                          stu.getNo());
+                  flag = true;
+
+                  if (!flag) {
+                     System.out.println("해당하는 학생의 이름이 없습니다");
+
+                  }
+               }
+            }
+         } else if (input.equals("n")) {
+            break;
+         }
+      }
+
+
+/*      while (input.equals("y")) {
+         System.out.println(stu1.getName());
+      }*/
+      System.out.println("종료되었습니다.");
+      //Do-while문은 한번은 실행이 되어야할때 쓰는것
+
+
    }
+
 }
 
