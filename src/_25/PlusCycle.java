72f160fee0ab09b25579d873066c6b2577403af6
bongj9 <lollypark501@naver.com>
2023-09-28 13:14:10 +0900

    삼각달팽이미완성

--- src/_25/PlusCycle.java
+++ src/_25/PlusCycle.java
@@ -1,7 +1,17 @@
 package _25;
 
 public class PlusCycle {
-    public static void main(String[] args) {
+    public int[] solution(int n){
+        int x = 0;
+        int y = 0;
+        int[][] triangle = new int[n][n];
+        int v = 1;
 
+        while (true) {
+            triangle[y][x] = v++;
+            if (y + 1 == n || triangle[y + 1][x] != 0) break;
+            y += 1; //
+            }
+        }
     }
 }
